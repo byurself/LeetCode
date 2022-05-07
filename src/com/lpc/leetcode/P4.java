32833cba671d731b3360e8e6101b836f5085af71
byu_rself <1870287004@qq.com>
2022-05-07 23:49:19 +0800

    [P4]寻找两个正序数组的中位数

---
+++ src/com/lpc/leetcode/P4.java
@@ -0,0 +1,111 @@
+package com.lpc.leetcode;
+
+import org.junit.Test;
+
+/**
+ * 寻找两个正序数组的中位数
+ *
+ * @author byu_rself
+ * @date 2022/5/7 23:42
+ */
+public class P4 {
+
+    @Test
+    public void solution() {
+        int[] nums1 = new int[]{1, 2, 3};
+        int[] nums2 = new int[]{2, 4, 9, 10};
+        System.out.println(findMedianSortedArrays(nums1, nums2));
+    }
+
+    /**
+     * 递归
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int left = (n + m + 1) / 2;
+        int right = (n + m + 2) / 2;
+        // 将偶数和奇数的情况合并，如果是奇数，会求两次同样的 k 。
+        return (getKth(nums1, 0, n - 1, nums2, 0, m - 1, left) + getKth(nums1, 0, n - 1, nums2, 0, m - 1, right)) * 0.5;
+    }
+
+    private int getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
+        int len1 = end1 - start1 + 1;
+        int len2 = end2 - start2 + 1;
+        // 让 len1 的长度小于 len2，这样就能保证如果有数组空了，一定是 len1
+        if (len1 > len2) return getKth(nums2, start2, end2, nums1, start1, end1, k);
+        if (len1 == 0) return nums2[start2 + k - 1];
+
+        if (k == 1) return Math.min(nums1[start1], nums2[start2]);
+
+        int i = start1 + Math.min(len1, k / 2) - 1;
+        int j = start2 + Math.min(len2, k / 2) - 1;
+
+        if (nums1[i] > nums2[j]) {
+            return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
+        } else {
+            return getKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
+        }
+    }
+
+    /**
+     * 合并数组
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public double findMedianSortedArrays1(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        if (m == 0) {
+            if (n % 2 == 0) {
+                return (nums2[n / 2 - 1] + nums2[n / 2]) / 2.0;
+            } else {
+                return nums2[n / 2];
+            }
+        }
+        if (n == 0) {
+            if (m % 2 == 0) {
+                return (nums1[m / 2 - 1] + nums1[m / 2]) / 2.0;
+            } else {
+                return nums1[m / 2];
+            }
+        }
+
+        int[] nums = new int[m + n];
+        int i = 0, j = 0;
+        int count = 0;
+        while (count < m + n) {
+            if (i == m) {
+                while (j < n) {
+                    nums[count++] = nums2[j++];
+                }
+                break;
+            }
+            if (j == n) {
+                while (i < m) {
+                    nums[count++] = nums1[i++];
+                }
+                break;
+            }
+
+            if (nums1[i] < nums2[j]) {
+                nums[count++] = nums1[i++];
+            } else {
+                nums[count++] = nums2[j++];
+            }
+        }
+
+        if (count % 2 == 0) {
+            return (nums[count / 2 - 1] + nums[count / 2]) / 2.0;
+        } else {
+            return nums[count / 2];
+        }
+    }
+
+}

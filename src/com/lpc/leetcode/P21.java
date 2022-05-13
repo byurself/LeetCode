f1480beb5bd3f919f372d2a20e0ed23cd9b01438
byu_rself <1870287004@qq.com>
2022-05-13 11:35:41 +0800

    [P21][Method 2]Merge Two Sorted Lists

--- src/com/lpc/leetcode/P21.java
+++ src/com/lpc/leetcode/P21.java
@@ -14,7 +14,7 @@ public class P21 {
     public void solution() {
         ListNode listNode1 = new ListNode(1, new ListNode(2, new ListNode(4)));
         ListNode listNode2 = new ListNode(1, new ListNode(3, new ListNode(4)));
-        ListNode listNode = mergeTwoLists(listNode1, listNode2);
+        ListNode listNode = mergeTwoLists1(listNode1, listNode2);
         while (listNode != null) {
             System.out.println(listNode.val);
             listNode = listNode.next;
@@ -22,7 +22,33 @@ public class P21 {
     }
 
     /**
+     * 迭代
+     *
+     * @param listNode1
+     * @param listNode2
+     * @return
+     */
+    public ListNode mergeTwoLists1(ListNode listNode1, ListNode listNode2) {
+        ListNode listNode = new ListNode();
+        ListNode p = listNode;
+        while (listNode1 != null && listNode2 != null) {
+            if (listNode1.val < listNode2.val) {
+                p.next = listNode1;
+                listNode1 = listNode1.next;
+            } else {
+                p.next = listNode2;
+                listNode2 = listNode2.next;
+            }
+            p = p.next;
+        }
+        // 合并后 listNode1 和 listNode2 最多只有一个还未被合并完，直接将链表末尾指向未合并完的链表即可
+        p.next = listNode1 == null ? listNode2 : listNode1;
+        return listNode.next;
+    }
+
+    /**
      * 递归
+     *
      * @param listNode1
      * @param listNode2
      * @return
@@ -40,7 +66,6 @@ public class P21 {
             return listNode2;
         }
     }
-
 
     private static class ListNode {
         int val;

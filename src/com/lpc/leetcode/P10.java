c3ac986d8bcbdc8cf37df7c2ac253f59b66e1060
byu_rself <1870287004@qq.com>
2022-05-18 11:21:51 +0800

    [P10][*]Regular Expression Matching

---
+++ src/com/lpc/leetcode/P10.java
@@ -0,0 +1,52 @@
+package com.lpc.leetcode;
+
+import org.junit.Test;
+
+/**
+ * Regular Expression Matching
+ *
+ * @author byu_rself
+ * @date 2022/5/18 11:06
+ */
+public class P10 {
+
+    @Test
+    public void solution() {
+        String s = "aa";
+        String p = "a*";
+        System.out.println(isMatch(s, p));
+    }
+
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+
+        boolean[][] f = new boolean[m + 1][n + 1];
+        f[0][0] = true;
+        for (int i = 0; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    f[i][j] = f[i][j - 2];
+                    if (matches(s, p, i, j - 1)) {
+                        f[i][j] = f[i][j] || f[i - 1][j];
+                    }
+                } else {
+                    if (matches(s, p, i, j)) {
+                        f[i][j] = f[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        return f[m][n];
+    }
+
+    public boolean matches(String s, String p, int i, int j) {
+        if (i == 0) {
+            return false;
+        }
+        if (p.charAt(j - 1) == '.') {
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);
+    }
+}

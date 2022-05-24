7fa915d8d308114dc16c0b6fba085e6facadddc7
byu_rself <1870287004@qq.com>
2022-05-24 09:25:14 +0800

    [P965]Univalued Binary Tree

---
+++ src/com/lpc/leetcode/P965.java
@@ -0,0 +1,53 @@
+package com.lpc.leetcode;
+
+import org.junit.Test;
+
+/**
+ * Univalued Binary Tree
+ *
+ * @author byu_rself
+ * @date 2022/5/24 9:14
+ */
+public class P965 {
+
+    @Test
+    public void solution() {
+
+    }
+
+    public boolean isUnivalTree(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (root.left != null) {
+            if (root.val != root.left.val || !isUnivalTree(root.left)) {
+                return false;
+            }
+        }
+        if (root.right != null) {
+            if (root.val != root.right.val || !isUnivalTree(root.right)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}

d995a470cd887a27c4812ce6a62ff4467a0b7666
byu_rself <1870287004@qq.com>
2022-06-02 21:36:29 +0800

    [P450]Delete Node in a BST

---
+++ src/com/lpc/leetcode/P450.java
@@ -0,0 +1,67 @@
+package com.lpc.leetcode;
+
+import org.junit.Test;
+
+/**
+ * Delete Node in a BST
+ *
+ * @author byu_rself
+ * @date 2022/6/2 21:32
+ */
+public class P450 {
+
+    @Test
+    public void solution() {
+
+    }
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+            return root;
+        } else if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+            return root;
+        } else {
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+            if (root.left == null) {
+                return root.right;
+            }
+            TreeNode successor = root.right;
+            while (successor.left != null) {
+                successor = successor.left;
+            }
+            root.right = deleteNode(root.right, successor.val);
+            successor.right = root.right;
+            successor.left = root.left;
+            return successor;
+        }
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}

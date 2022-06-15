a82a18803d86b8c5f7910a7d29bdd6bca0492cd7
byu_rself <1870287004@qq.com>
2022-06-15 19:59:32 +0800

    [P719]找出第K小的数对距离

---
+++ src/com/lpc/leetcode/P719.java
@@ -0,0 +1,53 @@
+package com.lpc.leetcode;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * 找出第K小的数对距离
+ *
+ * @author byu_rself
+ * @date 2022/6/15 19:55
+ */
+public class P719 {
+
+    @Test
+    public void solution() {
+        int[] nums = new int[]{1, 3, 1};
+        int k = 1;
+        System.out.println(smallestDistancePair(nums, k));
+    }
+
+    public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length, left = 0, right = nums[n - 1] - nums[0];
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                int i = binarySearch(nums, j, nums[j] - mid);
+                cnt += j - i;
+            }
+            if (cnt >= k) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public int binarySearch(int[] nums, int end, int target) {
+        int left = 0, right = end;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}

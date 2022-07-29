6d78125c0b41ceca89b54ece597253426b2f348e
byu_rself <1870287004@qq.com>
2022-07-29 13:22:41 +0800

    [P2325]解密消息

---
+++ src/com/lpc/leetcode/P2325.java
@@ -0,0 +1,34 @@
+package com.lpc.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 解密消息
+ *
+ * @author byu_rself
+ * @date 2022/7/29 13:16
+ */
+public class P2325 {
+
+    public String decodeMessage(String key, String message) {
+        Map<Character, Character> map = new HashMap<>();
+        char t = 'a';
+        for (int i = 0; i < key.length(); i++) {
+            char c = key.charAt(i);
+            if (!map.containsKey(c) && c != ' ') {
+                map.put(c, t++);
+            }
+        }
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < message.length(); i++) {
+            char c = message.charAt(i);
+            if (c == ' ') {
+                sb.append(' ');
+            } else {
+                sb.append(map.get(c));
+            }
+        }
+        return sb.toString();
+    }
+}

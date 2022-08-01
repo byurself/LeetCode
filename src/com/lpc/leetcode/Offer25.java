9e7f71e4240b9e71393d9497ec6bffc8e2643de9
byu_rself <1870287004@qq.com>
2022-08-01 09:16:11 +0800

    [Offer25]合并两个有序链表

---
+++ src/com/lpc/leetcode/Offer25.java
@@ -0,0 +1,48 @@
+package com.lpc.leetcode;
+
+/**
+ * 合并两个有序链表
+ * @author byu_rself
+ * @date 2022/8/1 8:58
+ */
+public class Offer25 {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0), p = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        p.next = l1 != null ? l1 : l2;
+        return head.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}

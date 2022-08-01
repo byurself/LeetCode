9dda6558e473fe35aeb73cba29395e5cb3b6ea6f
byu_rself <1870287004@qq.com>
2022-08-01 10:33:44 +0800

    [Offer022]链表中倒数第k个节点

---
+++ src/com/lpc/leetcode/Offer022.java
@@ -0,0 +1,53 @@
+package com.lpc.leetcode;
+
+/**
+ * 链表中倒数第k个节点
+ *
+ * @author byu_rself
+ * @date 2022/8/1 10:22
+ */
+public class Offer022 {
+
+    /**
+     * 快慢指针
+     */
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode slow = head, fast = head;
+        while (k-- > 0) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+    /**
+     * 遍历链表，差值法
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode getKthFromEnd1(ListNode head, int k) {
+        ListNode p = head;
+        int n = 0;
+        while (p != null) {
+            n++;
+            p = p.next;
+        }
+        for(p = head; n > k; n--) {
+            p = p.next;
+        }
+        return p;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}

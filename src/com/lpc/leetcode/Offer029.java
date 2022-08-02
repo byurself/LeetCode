575fc93aba305186862adb357e937d744a97e6ca
byu_rself <1870287004@qq.com>
2022-08-02 14:44:04 +0800

    [Offer029]排序的循环链表

--- src/com/lpc/leetcode/Offer029.java
+++ src/com/lpc/leetcode/Offer029.java
@@ -15,6 +15,29 @@ public class Offer029 {
 
     }
 
+    public Node insert1(Node head, int x) {
+        Node t = new Node(x);
+        t.next = t;
+        if (head == null) return t;
+        Node ans = head;
+        int min = head.val, max = head.val;
+        while (head.next != ans) {
+            head = head.next;
+            min = Math.min(min, head.val);
+            max = Math.max(max, head.val);
+        }
+        if (min == max) {
+            t.next = ans.next;
+            ans.next = t;
+        } else {
+            while (!(head.val == max && head.next.val == min)) head = head.next;
+            while (!(x <= min || x >= max) && !(head.val <= x && x <= head.next.val)) head = head.next;
+            t.next = head.next;
+            head.next = t;
+        }
+        return ans;
+    }
+
     public Node insert(Node head, int insertVal) {
         Node node = new Node(insertVal);
         if (head == null) {

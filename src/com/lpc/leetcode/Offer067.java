2bb86aca9819cfe770bf0ee9dc4963582d216dcc
byu_rself <1870287004@qq.com>
2022-08-16 10:38:14 +0800

    [Offer067]最大的异或

---
+++ src/com/lpc/leetcode/Offer067.java
@@ -0,0 +1,89 @@
+package com.lpc.leetcode;
+
+import org.junit.Test;
+
+/**
+ * 最大的异或
+ *
+ * @author byu_rself
+ * @date 2022/8/16 10:36
+ */
+public class Offer067 {
+
+    @Test
+    public void solution() {
+        int[] nums = new int[]{3, 10, 5, 25, 2, 8};
+        System.out.println(findMaximumXOR(nums));
+    }
+
+    // 字典树的根节点
+    Trie root = new Trie();
+    // 最高位的二进制位编号为 30
+    static final int HIGH_BIT = 30;
+
+    public int findMaximumXOR(int[] nums) {
+        int n = nums.length;
+        int x = 0;
+        for (int i = 1; i < n; ++i) {
+            // 将 nums[i-1] 放入字典树，此时 nums[0 .. i-1] 都在字典树中
+            add(nums[i - 1]);
+            // 将 nums[i] 看作 ai，找出最大的 x 更新答案
+            x = Math.max(x, check(nums[i]));
+        }
+        return x;
+    }
+
+    public void add(int num) {
+        Trie cur = root;
+        for (int k = HIGH_BIT; k >= 0; --k) {
+            int bit = (num >> k) & 1;
+            if (bit == 0) {
+                if (cur.left == null) {
+                    cur.left = new Trie();
+                }
+                cur = cur.left;
+            } else {
+                if (cur.right == null) {
+                    cur.right = new Trie();
+                }
+                cur = cur.right;
+            }
+        }
+    }
+
+    public int check(int num) {
+        Trie cur = root;
+        int x = 0;
+        for (int k = HIGH_BIT; k >= 0; --k) {
+            int bit = (num >> k) & 1;
+            if (bit == 0) {
+                // a_i 的第 k 个二进制位为 0，应当往表示 1 的子节点 right 走
+                if (cur.right != null) {
+                    cur = cur.right;
+                    x = x * 2 + 1;
+                } else {
+                    cur = cur.left;
+                    x = x * 2;
+                }
+            } else {
+                // a_i 的第 k 个二进制位为 1，应当往表示 0 的子节点 left 走
+                if (cur.left != null) {
+                    cur = cur.left;
+                    x = x * 2 + 1;
+                } else {
+                    cur = cur.right;
+                    x = x * 2;
+                }
+            }
+        }
+        return x;
+    }
+
+
+    class Trie {
+        // 左子树指向表示 0 的子节点
+        Trie left = null;
+        // 右子树指向表示 1 的子节点
+        Trie right = null;
+    }
+}

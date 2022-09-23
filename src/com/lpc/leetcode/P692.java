de61285d087beddf5614759d36b2b37248113042
byu_rself <1870287004@qq.com>
2022-09-23 11:19:42 +0800

    [P692]前K个高频单词

--- src/com/lpc/leetcode/P692.java
+++ src/com/lpc/leetcode/P692.java
@@ -25,6 +25,36 @@ public class P692 {
         for (String word : words) {
             map.put(word, map.getOrDefault(word, 0) + 1);
         }
+        PriorityQueue<String> q = new PriorityQueue<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (!map.get(o1).equals(map.get(o2))) return map.get(o1) - map.get(o2);
+                else return o2.compareTo(o1);
+            }
+        });
+
+        for (String s : map.keySet()) {
+            q.offer(s);
+            if (q.size() > k) {
+                q.poll();
+            }
+        }
+
+        List<String> list = new ArrayList<>();
+        while (q.size() > 0) {
+            list.add(q.poll());
+        }
+
+        Collections.reverse(list);
+        return list;
+    }
+
+
+    public List<String> topKFrequent1(String[] words, int k) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
         PriorityQueue<String> q = new PriorityQueue<>((a, b) -> {
             if (map.get(a).equals(map.get(b))) return b.compareTo(a); // 字符频率出现相同，按字典序排序
             else return map.get(a) - map.get(b); // 字符频率出现不同，按频率排序

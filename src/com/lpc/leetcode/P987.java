042713e27a809bf44e8794d1b2ff401ce0e42394
byu_rself <1870287004@qq.com>
2022-09-24 19:31:28 +0800

    [P987]二叉树的垂序遍历

--- src/com/lpc/leetcode/P987.java
+++ src/com/lpc/leetcode/P987.java
@@ -18,6 +18,40 @@ public class P987 {
         list.sort(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
+                if (o1[1] != o2[1]) return o1[1] - o2[1];
+                else if (o1[0] != o2[0]) return o1[0] - o2[0];
+                else return o1[2] - o2[2];
+            }
+        });
+
+        List<List<Integer>> ans = new ArrayList<>();
+        int lastCol = Integer.MIN_VALUE, index = 0;
+        for (int[] e : list) {
+            int row = e[0], column = e[1], val = e[2];
+            if (lastCol != column) {
+                ans.add(new ArrayList<>());
+                index++;
+                lastCol = column;
+            }
+            ans.get(index - 1).add(val);
+        }
+
+        return ans;
+    }
+
+    private void dfs(TreeNode node, int row, int column, List<int[]> list) {
+        if (node == null) return;
+        list.add(new int[]{row, column, node.val});
+        dfs(node.left, row + 1, column - 1, list);
+        dfs(node.right, row + 1, column + 1, list);
+    }
+
+    public List<List<Integer>> verticalTraversal1(TreeNode root) {
+        List<int[]> list = new ArrayList<>();
+        dfs(root, 0, 0, list);
+        list.sort(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
                 if (o1[1] != o2[1]) return o1[1] - o2[1]; // 列号从小到大
                 else if (o1[0] != o2[0]) return o1[0] - o2[0]; // 同列节点，行号从小到大
                 else return o1[2] - o2[2]; // 同行同列节点，值从小到大
@@ -38,7 +72,7 @@ public class P987 {
         return ans;
     }
 
-    private void dfs(TreeNode node, int row, int column, List<int[]> list) {
+    private void dfs1(TreeNode node, int row, int column, List<int[]> list) {
         if (node == null) return;
         list.add(new int[]{row, column, node.val});
         dfs(node.left, row + 1, column - 1, list);

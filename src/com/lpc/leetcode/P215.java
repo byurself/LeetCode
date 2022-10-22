35a4bfee33ec37ca90d555c6d041f8ea66da3555
byu_rself <1870287004@qq.com>
2022-10-22 13:05:18 +0800

    [P215]数组中的第K个最大元素

--- src/com/lpc/leetcode/P215.java
+++ src/com/lpc/leetcode/P215.java
@@ -11,6 +11,18 @@ import java.util.PriorityQueue;
 public class P215 {
 
     public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for (int i = 0; i < k; i++) q.offer(nums[i]);
+        for (int i = k; i < nums.length; i++) {
+            if (q.peek() < nums[i]) {
+                q.poll();
+                q.offer(nums[i]);
+            }
+        }
+        return q.peek();
+    }
+
+    public int findKthLargest1(int[] nums, int k) {
         PriorityQueue<Integer> q = new PriorityQueue<>(k);
         for (int i = 0; i < k; i++) q.offer(nums[i]);
         for (int i = k; i < nums.length; i++) {

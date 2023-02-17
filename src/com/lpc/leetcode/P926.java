a5ca4bf021d7f9131507630b168d4bb20d2fa3d0
byu_rself <1870287004@qq.com>
2023-02-17 16:06:34 +0800

    [P926]将字符串翻转到单调递增

--- src/com/lpc/leetcode/P926.java
+++ src/com/lpc/leetcode/P926.java
@@ -3,7 +3,7 @@ package com.lpc.leetcode;
 import org.junit.Test;
 
 /**
- * Flip String to Monotone Increasing
+ * 将字符串翻转到单调递增
  *
  * @author byu_rself
  * @date 2022/6/11 21:53
@@ -16,7 +16,33 @@ public class P926 {
         System.out.println(minFlipsMonoIncr(s));
     }
 
+    public int minFlipsMonoIncr1(String s) {
+        int n = s.length();
+        int dp0 = 0, dp1 = 0;
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            int dp0New = dp0 + (c == '1' ? 1 : 0);
+            int dp1New = Math.min(dp0, dp1) + (c == '0' ? 1 : 0);
+            dp0 = dp0New;
+            dp1 = dp1New;
+        }
+        return Math.min(dp0, dp1);
+    }
+
     public int minFlipsMonoIncr(String s) {
+        int n = s.length();
+        int[][] dp = new int[n + 1][2];
+        dp[0][0] = 0;
+        dp[0][1] = 0;
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            dp[i + 1][0] = dp[i][0] + (c == '1' ? 1 : 0);
+            dp[i + 1][1] = Math.min(dp[i][0], dp[i][1]) + (c == '0' ? 1 : 0);
+        }
+        return Math.min(dp[n][0], dp[n][1]);
+    }
+
+    public int minFlipsMonoIncr0(String s) {
         int n = s.length();
         int dp0 = 0, dp1 = 0;
         for (int i = 0; i < n; i++) {

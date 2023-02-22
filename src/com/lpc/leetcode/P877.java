1de8718c78780474c245532f2d000e0fbcf85649
byu_rself <1870287004@qq.com>
2023-02-22 11:24:47 +0800

    [P877]石子游戏

---
+++ src/com/lpc/leetcode/P877.java
@@ -0,0 +1,24 @@
+package com.lpc.leetcode;
+
+/**
+ * 石子游戏
+ *
+ * @author byu_rself
+ * @date 2023/2/22 11:15
+ */
+public class P877 {
+
+    public boolean stoneGame(int[] piles) {
+        int n = piles.length;
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = piles[i];
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][n - 1] > 0;
+    }
+}

019fede834c9ab53b1545fbcb6d9c6357321634e
byu_rself <1870287004@qq.com>
2023-03-18 14:47:21 +0800

    [P503]下一个更大元素 II

--- src/com/lpc/leetcode/P503.java
+++ src/com/lpc/leetcode/P503.java
@@ -1,5 +1,7 @@
 package com.lpc.leetcode;
 
+import org.junit.Test;
+
 import java.util.*;
 
 /**
@@ -10,7 +12,26 @@ import java.util.*;
  */
 public class P503 {
 
+    @Test
+    public void solution() {
+        int[] nums = new int[]{1, 2, 1};
+        System.out.println(Arrays.toString(nextGreaterElements(nums)));
+    }
+
     public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 2 * n - 1; i >= 0; --i) {
+            int cur = nums[i % n];
+            while (!stack.isEmpty() && cur >= nums[stack.peek()]) stack.pop();
+            ans[i % n] = stack.isEmpty() ? -1 : nums[stack.peek()];
+            stack.push(i % n);
+        }
+        return ans;
+    }
+
+    public int[] nextGreaterElements1(int[] nums) {
         int n = nums.length;
         int[] ans = new int[n];
         Arrays.fill(ans, -1);

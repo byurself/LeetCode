5f9b35cd8ae6dedfe3917e185c9a3bdf3eb3f813
byu_rself <1870287004@qq.com>
2023-03-21 14:08:29 +0800

    [P827]最大人工岛

--- src/com/lpc/leetcode/P827.java
+++ src/com/lpc/leetcode/P827.java
@@ -1,8 +1,6 @@
 package com.lpc.leetcode;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 /**
@@ -13,54 +11,72 @@ import java.util.Set;
  */
 public class P827 {
 
-    private static int[] d = {0, -1, 0, 1, 0};
+    private int[] p;
+    private int[] size;
+    private final int[] dirs = new int[]{-1, 0, 1, 0, -1};
+    private int n;
+
+    private int find(int x) {
+        if (p[x] != x) p[x] = find(p[x]);
+        return p[x];
+    }
+
+    private void union(int x, int y) {
+        int px = find(x), py = find(y);
+        if (px == py) return;
+        if (size[px] > size[py]) {
+            size[px] += size[py];
+            p[py] = p[px];
+        } else {
+            size[py] += size[px];
+            p[px] = p[py];
+        }
+    }
 
     public int largestIsland(int[][] grid) {
-        int n = grid.length, res = 0;
-        int[][] tag = new int[n][n];
-        Map<Integer, Integer> area = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1 && tag[i][j] == 0) {
-                    int t = i * n + j + 1;
-                    area.put(t, dfs(grid, i, j, tag, t));
-                    res = Math.max(res, area.get(t));
-                }
-            }
+        n = grid.length;
+        p = new int[n * n];
+        size = new int[n * n];
+        for (int i = 0; i < n * n; ++i) {
+            p[i] = i;
+            size[i] = 1;
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0) {
-                    int z = 1;
-                    Set<Integer> connected = new HashSet<>();
-                    for (int k = 0; k < 4; k++) {
-                        int x = i + d[k], y = j + d[k + 1];
-                        if (!valid(n, x, y) || tag[x][y] == 0 || connected.contains(tag[x][y])) {
-                            continue;
-                        }
-                        z += area.get(tag[x][y]);
-                        connected.add(tag[x][y]);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 0) continue;
+                for (int k = 0; k < 4; ++k) {
+                    int x = i + dirs[k], y = j + dirs[k + 1];
+                    if (x >= 0 && x < n && y >= 0 && y < n && grid[x][y] == 1) {
+                        union(getIndex(i, j), getIndex(x, y));
                     }
-                    res = Math.max(res, z);
                 }
             }
         }
-        return res;
-    }
-
-    public int dfs(int[][] grid, int x, int y, int[][] tag, int t) {
-        int n = grid.length, res = 1;
-        tag[x][y] = t;
-        for (int i = 0; i < 4; i++) {
-            int x1 = x + d[i], y1 = y + d[i + 1];
-            if (valid(n, x1, y1) && grid[x1][y1] == 1 && tag[x1][y1] == 0) {
-                res += dfs(grid, x1, y1, tag, t);
+        int ans = 1;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1) ans = Math.max(ans, size[find(getIndex(i, j))]);
+                else {
+                    int t = 1;
+                    Set<Integer> set = new HashSet<>();
+                    for (int k = 0; k < 4; ++k) {
+                        int x = i + dirs[k], y = j + dirs[k + 1];
+                        if (x >= 0 && x < n && y >= 0 && y < n && grid[x][y] == 1) {
+                            int root = find(getIndex(x, y));
+                            if (!set.contains(root)) {
+                                set.add(root);
+                                t += size[root];
+                            }
+                        }
+                    }
+                    ans = Math.max(ans, t);
+                }
             }
         }
-        return res;
+        return ans;
     }
 
-    public boolean valid(int n, int x, int y) {
-        return x >= 0 && x < n && y >= 0 && y < n;
+    private int getIndex(int i, int j) {
+        return i * n + j;
     }
 }

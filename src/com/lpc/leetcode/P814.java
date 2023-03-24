622e605633efa198454d82affb685250bee2cf2e
byu_rself <1870287004@qq.com>
2023-03-24 14:05:28 +0800

    [P814]二叉树剪枝

---
+++ src/com/lpc/leetcode/P814.java
@@ -0,0 +1,37 @@
+package com.lpc.leetcode;
+
+/**
+ * 二叉树剪枝
+ *
+ * @author byu_rself
+ * @date 2023/3/24 14:03
+ */
+public class P814 {
+
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) return null;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if (root.left == null && root.right == null && root.val == 0) return null;
+        return root;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}

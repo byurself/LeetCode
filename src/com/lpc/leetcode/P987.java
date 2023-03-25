bd257eccd641557b9d06748bf3192769e1e815dc
byu_rself <1870287004@qq.com>
2023-03-25 21:12:37 +0800

    [P987]二叉树的垂序遍历

--- src/com/lpc/leetcode/P987.java
+++ src/com/lpc/leetcode/P987.java
@@ -12,9 +12,39 @@ import java.util.List;
  */
 public class P987 {
 
+    List<int[]> list = new ArrayList<>();
+
     public List<List<Integer>> verticalTraversal(TreeNode root) {
+        dfs(root, 0, 0);
+        list.sort((a, b) -> {
+            if (a[1] != b[1]) return a[1] - b[1]; // 列号从小到大
+            else if (a[0] != b[0]) return a[0] - b[0]; // 同列节点，行号从小到大
+            else return a[2] - b[2]; // 同行同列节点，按值从小到大
+        });
+        List<List<Integer>> ans = new ArrayList<>();
+        int lastCol = Integer.MIN_VALUE, index = 0;
+        for (int[] a : list) {
+            int col = a[1], val = a[2];
+            if (lastCol != col) {
+                lastCol = col;
+                ans.add(new ArrayList<>());
+                ++index;
+            }
+            ans.get(index - 1).add(val);
+        }
+        return ans;
+    }
+
+    private void dfs(TreeNode node, int row, int col) {
+        if (node == null) return;
+        list.add(new int[]{row, col, node.val});
+        dfs(node.left, row + 1, col - 1);
+        dfs(node.right, row + 1, col + 1);
+    }
+
+    public List<List<Integer>> verticalTraversal2(TreeNode root) {
         List<int[]> list = new ArrayList<>();
-        dfs(root, 0, 0, list);
+        dfs2(root, 0, 0, list);
         list.sort(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
@@ -39,14 +69,14 @@ public class P987 {
         return ans;
     }
 
-    private void dfs(TreeNode node, int row, int column, List<int[]> list) {
+    private void dfs2(TreeNode node, int row, int column, List<int[]> list) {
         if (node == null) return;
         list.add(new int[]{row, column, node.val});
-        dfs(node.left, row + 1, column - 1, list);
-        dfs(node.right, row + 1, column + 1, list);
+        dfs2(node.left, row + 1, column - 1, list);
+        dfs2(node.right, row + 1, column + 1, list);
     }
 
-    public List<List<Integer>> verticalTraversal1(TreeNode root) {
+/*    public List<List<Integer>> verticalTraversal1(TreeNode root) {
         List<int[]> list = new ArrayList<>();
         dfs(root, 0, 0, list);
         list.sort(new Comparator<int[]>() {
@@ -77,7 +107,7 @@ public class P987 {
         list.add(new int[]{row, column, node.val});
         dfs(node.left, row + 1, column - 1, list);
         dfs(node.right, row + 1, column + 1, list);
-    }
+    }*/
 
     private static class TreeNode {
         int val;

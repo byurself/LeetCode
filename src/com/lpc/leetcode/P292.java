68962c286505a972a983fe544cd13e100a3e9902
byu_rself <1870287004@qq.com>
2023-03-31 16:30:03 +0800

    [P292]Nim 游戏

---
+++ src/com/lpc/leetcode/P292.java
@@ -0,0 +1,14 @@
+package com.lpc.leetcode;
+
+/**
+ * Nim 游戏
+ *
+ * @author byu_rself
+ * @date 2023/3/31 16:25
+ */
+public class P292 {
+
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}

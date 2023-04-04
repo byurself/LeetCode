d2bd3b71d14030f445b3757c748a64dd541a2820
byu_rself <1870287004@qq.com>
2023-04-04 13:03:10 +0800

    [P1000]合并石头的最低成本

--- src/com/lpc/leetcode/P1000.java
+++ src/com/lpc/leetcode/P1000.java
@@ -10,7 +10,38 @@ import java.util.Arrays;
  */
 public class P1000 {
 
-    int[][][] cache;
+    int[][] cache;
+    int[] s;
+    int k;
+
+    public int mergeStones(int[] stones, int k) {
+        int n = stones.length;
+        if ((n - 1) % (k - 1) > 0) return -1; // 无法合并成一堆
+        s = new int[n + 1];
+        for (int i = 1; i <= n; ++i) s[i] = s[i - 1] + stones[i - 1];
+        this.k = k;
+        cache = new int[n][n];
+        for (int i = 0; i < n; ++i) {
+            Arrays.fill(cache[i], -1); // -1表示还没计算过
+        }
+        return dfs(0, n - 1);
+    }
+
+    private int dfs(int i, int j) {
+        if (i == j) return 0;
+        if (cache[i][j] != -1) return cache[i][j];
+        int ans = Integer.MAX_VALUE;
+        for (int m = i; m < j; m += k - 1) {
+            ans = Math.min(ans, dfs(i, m) + dfs(m + 1, j));
+        }
+        if ((j - i) % (k - 1) == 0) {
+            // 可以合并成一堆
+            ans += s[j + 1] - s[i];
+        }
+        return cache[i][j] = ans;
+    }
+
+/*    int[][][] cache;
     int[] s;
     int k;
 
@@ -29,9 +60,9 @@ public class P1000 {
         return dfs(0, n - 1, 1);
     }
 
-    /**
+    *//**
      * 表示把stones[i]~stones[j]合并成p堆的最低成本
-     */
+     *//*
     private int dfs(int i, int j, int p) {
         if (cache[i][j][p] != -1) return cache[i][j][p];
         // 合并成一堆
@@ -45,5 +76,5 @@ public class P1000 {
             ans = Math.min(ans, dfs(i, m, 1) + dfs(m + 1, j, p - 1));
         }
         return cache[i][j][p] = ans;
-    }
+    }*/
 }

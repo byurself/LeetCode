c6843689b3694826804abf2eb607b4087bc5db7f
byu_rself <1870287004@qq.com>
2023-04-04 13:47:15 +0800

    [P1034]边界着色

--- src/com/lpc/leetcode/P1034.java
+++ src/com/lpc/leetcode/P1034.java
@@ -11,7 +11,40 @@ import java.util.Deque;
  */
 public class P1034 {
 
+    int m, n, color;
+    int[][] grid, ans;
+    int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
     public int[][] colorBorder(int[][] grid, int row, int col, int color) {
+        this.grid = grid;
+        this.color = color;
+        m = grid.length;
+        n = grid[0].length;
+        ans = new int[m][n];
+        dfs(row, col);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (ans[i][j] == 0) ans[i][j] = grid[i][j];
+            }
+        }
+        return ans;
+    }
+
+    private void dfs(int x, int y) {
+        int cnt = 0;
+        for (int[] dir : dirs) {
+            int nx = x + dir[0], ny = y + dir[1];
+            if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+            if (grid[x][y] != grid[nx][ny]) continue;
+            else ++cnt;
+            if (ans[nx][ny] != 0) continue;
+            ans[nx][ny] = -1; // 标记为使用过
+            dfs(nx, ny);
+        }
+        ans[x][y] = cnt == 4 ? grid[x][y] : color;
+    }
+
+/*    public int[][] colorBorder(int[][] grid, int row, int col, int color) {
         int m = grid.length, n = grid[0].length;
         int[][] ans = new int[m][n];
         int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
@@ -35,5 +68,5 @@ public class P1034 {
             }
         }
         return ans;
-    }
+    }*/
 }

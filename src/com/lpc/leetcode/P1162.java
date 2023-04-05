cfa2a3e462d150f6b78441ee6c8976cc5fa999c6
byu_rself <1870287004@qq.com>
2023-04-05 17:22:25 +0800

    [P1162]地图分析

--- src/com/lpc/leetcode/P1162.java
+++ src/com/lpc/leetcode/P1162.java
@@ -15,6 +15,8 @@ public class P1162 {
 
     int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
+
+
     public int maxDistance(int[][] grid) {
         int n = grid.length;
         Deque<int[]> queue = new ArrayDeque<>();
@@ -42,6 +44,36 @@ public class P1162 {
         }
         return ans;
     }
+
+    /*    public int maxDistance(int[][] grid) {
+        int n = grid.length;
+        Deque<int[]> queue = new ArrayDeque<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1) {
+                    queue.offer(new int[]{i, j});
+                    map.put(i * n + j, 0);
+                }
+            }
+        }
+        int ans = -1;
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int x = cur[0], y = cur[1];
+            int step = map.get(x * n + y);
+            for (int[] dir : dirs) {
+                int nx = x + dir[0], ny = y + dir[1];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue; // 越界
+                if (grid[nx][ny] != 0) continue; // 已经访问过
+                queue.offer(new int[]{nx, ny});
+                grid[nx][ny] = step + 1;
+                map.put(nx * n + ny, step + 1);
+                ans = Math.max(ans, step + 1);
+            }
+        }
+        return ans;
+    }*/
 
 /*    public int maxDistance(int[][] grid) {
         this.grid = grid;

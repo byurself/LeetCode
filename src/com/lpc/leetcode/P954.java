ef146e4f31dca93792e954da3bc20b73f58aa3d8
byu_rself <1870287004@qq.com>
2023-04-25 19:42:29 +0800

    [P954]二倍数对数组

--- src/com/lpc/leetcode/P954.java
+++ src/com/lpc/leetcode/P954.java
@@ -23,6 +23,28 @@ public class P954 {
 
     public boolean canReorderDoubled(int[] arr) {
         Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) map.merge(i, 1, Integer::sum);
+        List<Integer> list = new ArrayList<>(map.keySet());
+        list.sort((a, b) -> a - b);
+        for (int key : list) {
+            int cnt = map.get(key);
+            if (key > 0) {
+                if (map.getOrDefault(2 * key, 0) < cnt) return false;
+                if (cnt > 0) map.put(2 * key, map.get(2 * key) - cnt);
+            } else if (key == 0) {
+                if (cnt % 2 != 0) return false;
+            } else {
+                if (cnt > 0) {
+                    if (key % 2 != 0 || map.getOrDefault(key / 2, 0) < cnt) return false;
+                    map.put(key / 2, map.get(key / 2) - cnt);
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean canReorderDoubled1(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
         for (int n : arr) map.merge(n, 1, Integer::sum);
         List<Integer> list = new ArrayList<>(map.keySet());
         list.sort((a, b) -> a - b);

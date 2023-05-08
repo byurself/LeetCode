a84adc28400802fc808c129cd53e087ab1ee59c3
byu_rself <1870287004@qq.com>
2023-05-08 18:59:49 +0800

    [P857]雇佣K名工人的最低成本

--- src/com/lpc/leetcode/P857.java
+++ src/com/lpc/leetcode/P857.java
@@ -2,34 +2,38 @@ package com.lpc.leetcode;
 
 import java.util.Arrays;
 import java.util.PriorityQueue;
+import java.util.stream.IntStream;
 
 /**
- * 雇佣 K 名工人的最低成本
+ * 雇佣K名工人的最低成本
  *
  * @author byu_rself
  * @date 2022/9/11 14:02
  */
 public class P857 {
     public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
-        int n = quality.length;
-        Integer[] h = new Integer[n];
-        for (int i = 0; i < n; i++) h[i] = i;
-        Arrays.sort(h, (a, b) -> quality[b] * wage[a] - quality[a] * wage[b]);
-        double res = 1e9;
-        double totalq = 0.0;
-        PriorityQueue<Integer> q = new PriorityQueue<>((a, b) -> b - a);
-        for (int i = 0; i < k - 1; i++) {
-            totalq += quality[h[i]];
-            q.offer(quality[h[i]]);
+        int n = quality.length, sum = 0;
+        Integer[] index = IntStream.range(0, n).boxed().toArray(Integer[]::new);
+        // 等价于
+        // Integer[] index = new Integer[n];
+        // for (int i = 0; i < n; ++i) index[i] = i;
+
+        // 定义 ri = wage[i] / quality[i]
+        Arrays.sort(index, (a, b) -> wage[a] * quality[b] - wage[b] * quality[a]); // 按 r 升序
+        PriorityQueue<Integer> q = new PriorityQueue<>((a, b) -> b - a); // 大根堆
+        for (int i = 0; i < k; ++i) {
+            q.offer(quality[index[i]]);
+            sum += quality[index[i]];
         }
-        for (int i = k - 1; i < n; i++) {
-            int idx = h[i];
-            totalq += quality[idx];
-            q.offer(quality[idx]);
-            double totalc = ((double) wage[idx] / quality[idx]) * totalq;
-            res = Math.min(res, totalc);
-            totalq -= q.poll();
+        double ans = sum * ((double) wage[index[k - 1]] / quality[index[k - 1]]);
+        for (int i = k; i < n; ++i) {
+            int cur = quality[index[i]];
+            if (cur < q.peek()) {
+                sum -= q.poll() - cur;
+                q.offer(cur);
+                ans = Math.min(ans, sum * ((double) wage[index[i]] / cur));
+            }
         }
-        return res;
+        return ans;
     }
 }

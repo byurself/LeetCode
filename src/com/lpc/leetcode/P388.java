4401cb0437399c09aee9e6923fb9fe46665527ee
byu_rself <1870287004@qq.com>
2023-05-16 10:48:57 +0800

    [P388]文件的最长绝对路径

--- src/com/lpc/leetcode/P388.java
+++ src/com/lpc/leetcode/P388.java
@@ -1,5 +1,7 @@
 package com.lpc.leetcode;
 
+import org.junit.Test;
+
 import java.util.ArrayDeque;
 import java.util.Deque;
 
@@ -11,7 +13,39 @@ import java.util.Deque;
  */
 public class P388 {
 
+    @Test
+    public void solution() {
+        String input = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext";
+        System.out.println(lengthLongestPath(input));
+    }
+
     public int lengthLongestPath(String input) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int n = input.length(), ans = 0;
+        for (int i = 0; i < n; ) {
+            int depth = 0;
+            while (i < n && input.charAt(i) == '\t') {
+                ++i;
+                ++depth;
+            }
+            boolean isFile = false;
+            int length = 0;
+            while (i < n && input.charAt(i) != '\n') {
+                ++length;
+                if (input.charAt(i) == '.') isFile = true;
+                ++i;
+            }
+            ++i;
+            while (stack.size() > depth) stack.pop();
+            if (!stack.isEmpty()) length += stack.peek() + 1;
+
+            if (isFile) ans = Math.max(ans, length);
+            else stack.push(length);
+        }
+        return ans;
+    }
+
+    public int lengthLongestPath1(String input) {
         int n = input.length(), pos = 0, ans = 0;
         Deque<Integer> stack = new ArrayDeque<>();
         while (pos < n) {

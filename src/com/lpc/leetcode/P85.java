f4ac7f7a9338c97262e8a3ae9b5ab35d335e2a04
byu_rself <1870287004@qq.com>
2023-05-23 10:17:21 +0800

    [P85]最大矩形

--- src/com/lpc/leetcode/P85.java
+++ src/com/lpc/leetcode/P85.java
@@ -13,6 +13,44 @@ import java.util.Deque;
 public class P85 {
 
     public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) return 0;
+        int row = matrix.length, col = matrix[0].length;
+        int[] heights = new int[col];
+        int ans = 0;
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                heights[j] = matrix[i][j] == '0' ? 0 : heights[j] + 1;
+            }
+            ans = Math.max(ans, largestRectangleArea(heights));
+        }
+        return ans;
+    }
+
+    private int largestRectangleArea(int[] heights) {
+        int n = heights.length, ans = 0;
+        int[] l = new int[n], r = new int[n];
+        Arrays.fill(l, -1);
+        Arrays.fill(r, n);
+        Deque<Integer> stack = new ArrayDeque<>();
+        // 确定右边界
+        for (int i = 0; i < n; ++i) {
+            while (!stack.isEmpty() && heights[stack.peek()] > heights[i]) r[stack.pop()] = i;
+            stack.push(i);
+        }
+        stack.clear();
+        // 确定左边界
+        for (int i = n - 1; i >= 0; --i) {
+            while (!stack.isEmpty() && heights[stack.peek()] > heights[i]) l[stack.pop()] = i;
+            stack.push(i);
+        }
+        for (int i = 0; i < n; ++i) {
+            int height = heights[i], width = r[i] - l[i] - 1;
+            ans = Math.max(ans, height * width);
+        }
+        return ans;
+    }
+
+    public int maximalRectangle1(char[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
         int[][] sum = new int[m + 10][n + 10];
         for (int i = 1; i <= m; i++) {

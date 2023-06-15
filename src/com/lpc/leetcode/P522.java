ac74d4011c987674e08fb63ecd7c9d90ff64d3e1
byu_rself <1870287004@qq.com>
2023-06-15 14:24:47 +0800

    [P522]最长特殊序列II

--- src/com/lpc/leetcode/P522.java
+++ src/com/lpc/leetcode/P522.java
@@ -3,7 +3,7 @@ package com.lpc.leetcode;
 import org.junit.Test;
 
 /**
- * 最长特殊序列 II
+ * 最长特殊序列II
  *
  * @author byu_rself
  * @date 2022/6/27 12:53
@@ -17,31 +17,26 @@ public class P522 {
     }
 
     public int findLUSlength(String[] strs) {
-        int n = strs.length;
-        int ans = -1;
+        int n = strs.length, ans = -1;
         for (int i = 0; i < n; ++i) {
-            boolean check = true;
+            boolean flag = true;
             for (int j = 0; j < n; ++j) {
                 if (i != j && isSubseq(strs[i], strs[j])) {
-                    check = false;
+                    flag = false;
                     break;
                 }
             }
-            if (check) {
-                ans = Math.max(ans, strs[i].length());
-            }
+            if (flag) ans = Math.max(ans, strs[i].length());
         }
         return ans;
     }
 
-    public boolean isSubseq(String s, String t) {
-        int ptS = 0, ptT = 0;
-        while (ptS < s.length() && ptT < t.length()) {
-            if (s.charAt(ptS) == t.charAt(ptT)) {
-                ++ptS;
-            }
-            ++ptT;
+    private boolean isSubseq(String s, String t) {
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) ++i;
+            ++j;
         }
-        return ptS == s.length();
+        return i == s.length();
     }
 }

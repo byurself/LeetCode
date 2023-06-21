e28d0452c443989ceb11e9537ed3e0731158cd48
byu_rself <1870287004@qq.com>
2023-06-21 16:16:05 +0800

    [P1823]找出游戏的获胜者

--- src/com/lpc/leetcode/P1823.java
+++ src/com/lpc/leetcode/P1823.java
@@ -2,6 +2,9 @@ package com.lpc.leetcode;
 
 import org.junit.Test;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * 找出游戏的获胜者
  *
@@ -18,9 +21,23 @@ public class P1823 {
     }
 
     public int findTheWinner(int n, int k) {
+        Deque<Integer> queue = new ArrayDeque<>();
+        for (int i = 1; i <= n; ++i) {
+            queue.offer(i);
+        }
+        while (queue.size() > 1) {
+            for (int i = 1; i < k; ++i) {
+                queue.offer(queue.poll());
+            }
+            queue.poll();
+        }
+        return queue.peek();
+    }
+
+    /*public int findTheWinner(int n, int k) {
         if (n == 1) return 1;
         int ans = findTheWinner(n - 1, k) + k;
         return ans % n == 0 ? n : (ans % n);
-    }
+    }*/
 
 }

5db7e8e2b1920fbefcf37d480b1b58f5d26bc75f
byu_rself <1870287004@qq.com>
2023-07-09 16:40:52 +0800

    [P15]三数之和

--- src/com/lpc/leetcode/P15.java
+++ src/com/lpc/leetcode/P15.java
@@ -24,6 +24,31 @@ public class P15 {
     }
 
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < n - 2; ++i) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[j++], nums[k--]));
+                    // 去重
+                    while (j < k && nums[j] == nums[j - 1]) ++j;
+                    while (j < k && nums[k] == nums[k + 1]) --k;
+                } else if (sum < 0) {
+                    ++j;
+                } else {
+                    --k;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public List<List<Integer>> threeSum2(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
         if (nums == null || nums.length < 3) {
             return list;

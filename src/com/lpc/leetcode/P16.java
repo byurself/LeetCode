918f31c2d3d52cda9df5b7c41f7d96cfe4429276
byu_rself <1870287004@qq.com>
2023-07-10 09:11:23 +0800

    [P16]最接近的三数之和

--- src/com/lpc/leetcode/P16.java
+++ src/com/lpc/leetcode/P16.java
@@ -21,6 +21,22 @@ public class P16 {
 
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
+        int n = nums.length, ans = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < n - 2; ++i) {
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) return sum;
+                if (Math.abs(sum - target) < Math.abs(ans - target)) ans = sum;
+                if (sum < target) ++j;
+                else --k;
+            }
+        }
+        return ans;
+    }
+
+    public int threeSumClosest1(int[] nums, int target) {
+        Arrays.sort(nums);
         int result = nums[0] + nums[1] + nums[2];
         for (int i = 0; i < nums.length; i++) {
             int start = i + 1, end = nums.length - 1;

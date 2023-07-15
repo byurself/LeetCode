4b0b2bf349e6042d0856c510d9b0f4b57e46f690
byu_rself <1870287004@qq.com>
2023-07-15 14:37:10 +0800

    [P18]四数之和

--- src/com/lpc/leetcode/P18.java
+++ src/com/lpc/leetcode/P18.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * 4Sum
+ * 四数之和
  *
  * @author byu_rself
  * @date 2022/5/20 15:44
@@ -16,8 +16,8 @@ public class P18 {
 
     @Test
     public void solution() {
-        int[] nums = new int[]{1, 0, -1, 0, -2, 2};
-        int target = 0;
+        int[] nums = new int[]{0, 0, 0, -1000000000, -1000000000, -1000000000, -1000000000};
+        int target = -1000000000;
         List<List<Integer>> list = fourSum(nums, target);
         for (List<Integer> integers : list) {
             for (Integer integer : integers) {
@@ -28,6 +28,54 @@ public class P18 {
     }
 
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        if (n < 4) return ans;
+        Arrays.sort(nums);
+        for (int a = 0; a < n - 3; ++a) {
+            if (a > 0 && nums[a] == nums[a - 1]) continue;
+
+            long sum = (long) nums[a] + nums[a + 1] + nums[a + 2] + nums[a + 3];
+            if (sum > target) break;
+
+            sum = (long) nums[a] + nums[n - 3] + nums[n - 2] + nums[n - 1];
+            if (sum < target) continue;
+
+            for (int b = a + 1; b < n - 2; ++b) {
+                if (b > a + 1 && nums[b] == nums[b - 1]) continue;
+
+                sum = (long) nums[a] + nums[b] + nums[b + 1] + nums[b + 2];
+                if (sum > target) break;
+
+                sum = (long) nums[a] + nums[b] + nums[n - 2] + nums[n - 1];
+                if (sum < target) continue;
+
+                int c = b + 1, d = n - 1;
+                while (c < d) {
+                    sum = nums[a] + nums[b] + nums[c] + nums[d];
+                    if (sum == target) {
+                        ans.add(List.of(nums[a], nums[b], nums[c], nums[d]));
+                        while (c < d && nums[c] == nums[c + 1]) ++c;
+                        while (c < d && nums[d] == nums[d - 1]) --d;
+                        ++c;
+                        --d;
+                    } else if (sum < target) ++c;
+                    else --d;
+                }
+            }
+        }
+        return ans;
+    }
+
+
+
+
+
+
+
+
+
+    /*public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> list = new ArrayList<List<Integer>>();
         if (nums == null || nums.length < 4) {
             return list;
@@ -76,5 +124,5 @@ public class P18 {
             }
         }
         return list;
-    }
+    }*/
 }

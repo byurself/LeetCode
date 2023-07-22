e2b8dd7ba800e5b77fbf3045233dc7d3a86e8f77
byu_rself <1870287004@qq.com>
2023-07-22 18:05:57 +0800

    [P224]基本计算器

--- src/com/lpc/leetcode/P224.java
+++ src/com/lpc/leetcode/P224.java
@@ -1,5 +1,7 @@
 package com.lpc.leetcode;
 
+import org.junit.Test;
+
 import java.util.ArrayDeque;
 import java.util.Deque;
 
@@ -11,7 +13,53 @@ import java.util.Deque;
  */
 public class P224 {
 
-    int i;
+    @Test
+    public void solution() {
+        String s = "(1+(4+5+2)-3)+(6+8)";
+        System.out.println(calculate(s));
+    }
+
+    int i, n;
+    char[] cs;
+
+    public int calculate(String s) {
+        cs = s.toCharArray();
+        n = cs.length;
+        return cal();
+    }
+
+    private int cal() {
+        int num = 0;
+        char sign = '+';
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (; i < n; ++i) {
+            char c = cs[i];
+            if (c == '(') { // 开始递归
+                ++i;
+                num = cal();
+            } else if (c >= '0' && c <= '9') { // 数字
+                num = num * 10 + (c - '0');
+            }
+            if (!(c >= '0' && c <= '9') && c != ' ' || i == n - 1) {
+                // 符号
+                switch (sign) {
+                    case '+' -> stack.push(num);
+                    case '-' -> stack.push(-num);
+                    case '*' -> stack.push(stack.pop() * num);
+                    case '/' -> stack.push(stack.pop() / num);
+                }
+                sign = c;
+                num = 0;
+            }
+            // 结束递归
+            if (c == ')') break;
+        }
+        int ans = 0;
+        while (!stack.isEmpty()) ans += stack.pop();
+        return ans;
+    }
+
+    /*int i;
 
     public int calculate(String s) {
         return cal(s);
@@ -49,5 +97,5 @@ public class P224 {
             ans += stack.pop();
         }
         return ans;
-    }
+    }*/
 }

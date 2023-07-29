e970d86c7822a69087857c1a69005ccb4839e852
byu_rself <1870287004@qq.com>
2023-07-29 16:06:17 +0800

    [P141]环形链表

--- src/com/lpc/leetcode/P141.java
+++ src/com/lpc/leetcode/P141.java
@@ -9,6 +9,16 @@ package com.lpc.leetcode;
 public class P141 {
 
     public boolean hasCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+
+    public boolean hasCycle1(ListNode head) {
         if (head == null || head.next == null) return false;
         ListNode slow = head, fast = head.next;
         while (slow != fast) {

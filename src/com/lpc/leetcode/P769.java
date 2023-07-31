508cd0de4eaba2cfc18239edcdbc6df6d2e7f5be
byu_rself <1870287004@qq.com>
2023-07-31 10:56:59 +0800

    [P769]最多能完成排序的块

--- src/com/lpc/leetcode/P769.java
+++ src/com/lpc/leetcode/P769.java
@@ -9,10 +9,10 @@ package com.lpc.leetcode;
 public class P769 {
 
     public int maxChunksToSorted(int[] arr) {
-        int ans = 1, max = -1;
-        for (int i = 0; i < arr.length; i++) {
+        int ans = 0, max = -1;
+        for (int i = 0; i < arr.length; ++i) {
             max = Math.max(max, arr[i]);
-            if (max == i) ans++;
+            if (max == i) ++ans;
         }
         return ans;
     }

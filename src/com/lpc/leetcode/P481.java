cbe6a64e7c2a5e1c846bcfcadcf3593dcb14dd56
byu_rself <1870287004@qq.com>
2023-07-31 10:39:17 +0800

    [P481]神奇字符串

--- src/com/lpc/leetcode/P481.java
+++ src/com/lpc/leetcode/P481.java
@@ -13,6 +13,21 @@ import java.util.List;
 public class P481 {
 
     public int magicalString(int n) {
+        char[] s = new char[n + 2];
+        s[0] = 1;
+        s[1] = s[2] = 2;
+        char c = 2;
+        for (int i = 2, j = 3; j < n; ++i) {
+            c ^= 3; // 1^3=2, 2^3=1
+            s[j++] = c;
+            if (s[i] == 2) s[j++] = c;
+        }
+        int ans = 0;
+        for (int i = 0; i < n; ++i) ans += 2 - s[i];
+        return ans;
+    }
+
+    public int magicalString1(int n) {
         List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 2));
         int i = 2, ans = 0;
         while (list.size() < n) {

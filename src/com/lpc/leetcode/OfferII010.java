fc637c35a868351c32aca2bbee1d927c612876e0
byu_rself <1870287004@qq.com>
2023-08-02 10:57:27 +0800

    [OfferII010]和为k的子数组

--- src/com/lpc/leetcode/OfferII010.java
+++ src/com/lpc/leetcode/OfferII010.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 和为 k 的子数组
+ * 和为k的子数组
  *
  * @author byu_rself
  * @date 2022/8/22 14:04
@@ -21,13 +21,26 @@ public class OfferII010 {
     }
 
     public int subarraySum(int[] nums, int k) {
-        int ans = 0, pre = 0;
+        int ans = 0, n = nums.length;
+        for (int left = 0; left < n; ++left) {
+            int sum = 0;
+            for (int right = left; right >= 0; --right) {
+                sum += nums[right];
+                if (sum == k) ++ans;
+            }
+        }
+        return ans;
+    }
+
+    public int subarraySum1(int[] nums, int k) {
+        int ans = 0, preSum = 0;
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
         for (int num : nums) {
-            pre += num;
-            if (map.containsKey(pre - k)) ans += map.get(pre - k);
-            map.put(pre, map.getOrDefault(pre, 0) + 1);
+            preSum += num;
+            if (map.containsKey(preSum - k)) ans += map.get(preSum - k);
+            map.merge(preSum, 1, Integer::sum);
+            // map.put(preSum, map.getOrDefault(preSum, 0) + 1);
         }
         return ans;
     }

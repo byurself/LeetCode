06a680f9761af5b86b9e104f4b8290c7192509c0
byu_rself <1870287004@qq.com>
2023-08-04 10:40:49 +0800

    [Offer003]数组中重复的数字

--- src/com/lpc/leetcode/Offer003.java
+++ src/com/lpc/leetcode/Offer003.java
@@ -1,46 +1,25 @@
 package com.lpc.leetcode;
 
-import org.junit.Test;
-
 /**
- * 前 n 个数字二进制中 1 的个数
+ * 数组中重复的数字
  *
  * @author byu_rself
- * @date 2022/8/15 11:03
+ * @date 2023/8/4 10:40
  */
 public class Offer003 {
 
-    @Test
-    public void solution() {
-        int n = 85723;
-        int[] bits = countBits(n);
-        for (int bit : bits) {
-            System.out.print(bit + "\t");
-        }
-    }
-
-    /**
-     * 位运算
-     */
-    // 从小到大遍历
-    public int[] countBits(int n) {
-        int[] ans = new int[n + 1];
-        // ans[i] = 「i >> 1 所包含的 1 的个数」+「i 的最低位是否为 1」
-        for (int i = 1; i <= n; i++) ans[i] = ans[i >> 1] + (i & 1);
-        return ans;
-    }
-
-    public int[] countBits1(int n) {
-        int[] ans = new int[n + 1];
-        for (int i = 0; i <= n; i++) {
-            int count = 0, mod = 0, num = i;
-            while (num > 0) {
-                mod = num % 2;
-                if (mod == 1) count++;
-                num /= 2;
+    public int findRepeatNumber(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] == i) {
+                ++i;
+                continue;
             }
-            ans[i] = count;
+            if (nums[i] == nums[nums[i]]) return nums[i];
+            int temp = nums[i];
+            nums[i] = nums[temp];
+            nums[temp] = temp;
         }
-        return ans;
+        return -1;
     }
 }

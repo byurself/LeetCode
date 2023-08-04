b6209b7d51c8b6ccc64ad17b8f3745387ff50d28
byu_rself <1870287004@qq.com>
2023-08-04 11:02:02 +0800

    [P448]找到所有数组中消失的数字

--- src/com/lpc/leetcode/P448.java
+++ src/com/lpc/leetcode/P448.java
@@ -13,6 +13,26 @@ public class P448 {
 
     public List<Integer> findDisappearedNumbers(int[] nums) {
         int n = nums.length;
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] != i + 1 && nums[nums[i] - 1] == nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i + 1) ans.add(i + 1);
+        }
+        return ans;
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
+    public List<Integer> findDisappearedNumbers1(int[] nums) {
+        int n = nums.length;
         List<Integer> list = new ArrayList<>();
         for (int num : nums) {
             int index = (num - 1) % n;

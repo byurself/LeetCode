79b14a97aa77d3bf1522d637f6b482a9d79690be
byu_rself <1870287004@qq.com>
2023-08-08 10:46:04 +0800

    [P31]下一个排列

--- src/com/lpc/leetcode/P31.java
+++ src/com/lpc/leetcode/P31.java
@@ -9,38 +9,30 @@ package com.lpc.leetcode;
 public class P31 {
 
     public void nextPermutation(int[] nums) {
-        if (nums == null || nums.length == 0) return;
-        int length = nums.length;
-        int firstIndex = -1, secondIndex = -1;
-        for (int i = length - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                firstIndex = i;
-                break;
-            }
+        int n = nums.length, i = n - 2;
+        // 从后往前找到第一个 (i, i + 1) 满足 nums[i] < nums[i + 1]
+        while (i >= 0 && nums[i] >= nums[i + 1]) --i;
+        if (i >= 0) {
+            int j = n - 1;
+            // 在 [i + 1, n) 中从后往前找到第一个元素 j，满足nums[i] < nums[j]
+            while (j >= 0 && nums[j] <= nums[i]) --j;
+            // 交换 nums[i] 与 nums[j]
+            swap(nums, i, j);
         }
-        if (firstIndex == -1) {
-            reverse(nums, 0, length - 1);
-            return;
+        // 此时区间 [i + 1, n) 必为降序，对该区间元素反转使其升序
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int left) {
+        int right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left++, right--);
         }
-        for (int i = length - 1; i >= 0; i--) {
-            if (nums[i] > nums[firstIndex]) {
-                secondIndex = i;
-                break;
-            }
-        }
-        swap(nums, firstIndex, secondIndex);
-        reverse(nums, firstIndex + 1, length - 1);
     }
 
     private void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
+        int temp = nums[i];
         nums[i] = nums[j];
-        nums[j] = tmp;
-    }
-
-    private void reverse(int[] nums, int i, int j) {
-        while (i < j) {
-            swap(nums, i++, j--);
-        }
+        nums[j] = temp;
     }
 }

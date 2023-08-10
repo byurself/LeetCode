e96235afd54fe71f1d9bccc90bd8803b8d4a5d86
byu_rself <1870287004@qq.com>
2023-08-10 14:18:41 +0800

    [P312]戳气球

--- src/com/lpc/leetcode/P312.java
+++ src/com/lpc/leetcode/P312.java
@@ -9,19 +9,19 @@ package com.lpc.leetcode;
 public class P312 {
 
     public int maxCoins(int[] nums) {
-        int length = nums.length;
-        int[] arr = new int[length + 2];
-        arr[0] = arr[length + 1] = 1;
-        for (int i = 1; i <= length; i++) arr[i] = nums[i - 1];
-        int[][] dp = new int[length + 2][length + 2];
-        for (int len = 3; len <= length + 2; len++) {
-            for (int l = 0; l + len - 1 <= length + 1; l++) {
+        int n = nums.length;
+        int[] arr = new int[n + 2];
+        arr[0] = arr[n + 1] = 1;
+        for (int i = 1; i <= n; i++) arr[i] = nums[i - 1];
+        int[][] dp = new int[n + 2][n + 2];
+        for (int len = 3; len <= n + 2; ++len) {
+            for (int l = 0; l + len - 1 <= n + 1; ++l) {
                 int r = l + len - 1;
-                for (int k = l + 1; k <= r - 1; k++) {
-                    dp[l][r] = Math.max(dp[l][r], dp[l][k] + dp[k][r] + arr[l] * arr[k] * arr[r]);
+                for (int k = l + 1; k <= r - 1; ++k) {
+                    dp[l][r] = Math.max(dp[l][r], dp[l][k] + arr[l] * arr[k] * arr[r] + dp[k][r]);
                 }
             }
         }
-        return dp[0][length + 1];
+        return dp[0][n + 1];
     }
 }

1fb17977fd4f3ccd835fab29a92ec2ba74ae5e5e
byu_rself <1870287004@qq.com>
2023-08-21 09:59:47 +0800

    [P667]优美的排列II

--- src/com/lpc/leetcode/P667.java
+++ src/com/lpc/leetcode/P667.java
@@ -3,7 +3,7 @@ package com.lpc.leetcode;
 import org.junit.Test;
 
 /**
- * 优美的排列 II
+ * 优美的排列II
  *
  * @author byu_rself
  * @date 2022/9/8 9:49
@@ -12,7 +12,7 @@ public class P667 {
 
     @Test
     public void solution() {
-        int n = 3, k = 1;
+        int n = 6, k = 3;
         int[] ans = constructArray(n, k);
         for (int i : ans) {
             System.out.print(i + " ");
@@ -23,10 +23,10 @@ public class P667 {
         int[] ans = new int[n];
         int index = 0;
         // 前面部分相差为1
-        for (int i = 1; i < n - k; i++) {
+        for (int i = 1; i < n - k; ++i) {
             ans[index++] = i;
         }
-        for (int i = n - k, j = n; i <= j; i++, j--) {
+        for (int i = n - k, j = n; i <= j; ++i, --j) {
             ans[index++] = i;
             if (i != j) {
                 ans[index++] = j;

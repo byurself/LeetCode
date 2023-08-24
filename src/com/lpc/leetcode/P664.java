eaf94c3be7765d69eba1cba323f7724e6a566a86
byu_rself <1870287004@qq.com>
2023-08-24 11:27:30 +0800

    [P664]奇怪的打印机

---
+++ src/com/lpc/leetcode/P664.java
@@ -0,0 +1,27 @@
+package com.lpc.leetcode;
+
+/**
+ * 奇怪的打印机
+ *
+ * @author byu_rself
+ * @date 2023/8/24 11:24
+ */
+public class P664 {
+
+    public int strangePrinter(String s) {
+        int n = s.length();
+        int[][] f = new int[n + 1][n + 1];
+        for (int len = 1; len <= n; ++len) {
+            for (int l = 0; l + len - 1 < n; ++l) {
+                int r = l + len - 1;
+                f[l][r] = f[l + 1][r] + 1;
+                for (int k = l + 1; k <= r; ++k) {
+                    if (s.charAt(l) == s.charAt(k)) {
+                        f[l][r] = Math.min(f[l][r], f[l][k - 1] + f[k + 1][r]);
+                    }
+                }
+            }
+        }
+        return f[0][n - 1];
+    }
+}

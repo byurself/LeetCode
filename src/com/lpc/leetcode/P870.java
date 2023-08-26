47e326fb616bbad6fe879aa475bc107d541e6f3c
byu_rself <1870287004@qq.com>
2023-08-26 17:06:53 +0800

    [P870]优势洗牌

--- src/com/lpc/leetcode/P870.java
+++ src/com/lpc/leetcode/P870.java
@@ -11,6 +11,20 @@ import java.util.Arrays;
 public class P870 {
 
     public int[] advantageCount(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        Arrays.sort(nums1);
+        int[] ans = new int[n];
+        Integer[] index = new Integer[n];
+        for (int i = 0; i < n; ++i) index[i] = i;
+        Arrays.sort(index, (a, b) -> nums2[a] - nums2[b]);
+        int left = 0, right = n - 1;
+        for (int num : nums1) {
+            ans[num > nums2[index[left]] ? index[left++] : index[right--]] = num;
+        }
+        return ans;
+    }
+
+    public int[] advantageCount1(int[] nums1, int[] nums2) {
         int length = nums1.length;
         Arrays.sort(nums1);
         Integer[] index = new Integer[length];

cd7f6239035f9783b0fae1bdb67813b21b560fd2
byu_rself <1870287004@qq.com>
2023-09-05 11:30:11 +0800

    [P687]最长同值路径

--- src/com/lpc/leetcode/P687.java
+++ src/com/lpc/leetcode/P687.java
@@ -8,21 +8,21 @@ package com.lpc.leetcode;
  */
 public class P687 {
 
-    int res = 0;
+    int ans;
 
     public int longestUnivaluePath(TreeNode root) {
         dfs(root);
-        return res;
+        return ans;
     }
 
     private int dfs(TreeNode root) {
-        if (root == null) return 0;
-        int left = dfs(root.left), right = dfs(root.right);
-        int l = 0, r = 0;
-        if (root.left != null && root.left.val == root.val) l = left + 1;
-        if (root.right != null && root.right.val == root.val) r = right + 1;
-        res = Math.max(res, l + r); // 更新最长同值路径
-        return Math.max(l, r); // 返回结点对应的最长同值路径
+        if (root == null) return -1; // 下面+1后，对于叶子节点就刚好为0
+        int left = dfs(root.left) + 1; // 左子树最大链长+1
+        int right = dfs(root.right) + 1; // 右子树最大链长+1
+        if (root.left != null && root.left.val != root.val) left = 0; // 链长视为0
+        if (root.right != null && root.right.val != root.val) right = 0; // 链长视为0
+        ans = Math.max(ans, left + right);
+        return Math.max(left, right); // 当前子树最大链长
     }
 
     private static class TreeNode {

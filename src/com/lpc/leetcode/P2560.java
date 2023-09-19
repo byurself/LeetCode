50e9ca6e6efd64c7bf67d8784bf2a5e8f49d9239
byu_rself <1870287004@qq.com>
2023-09-19 10:01:26 +0800

    [P2560]打家劫舍IV

---
+++ src/com/lpc/leetcode/P2560.java
@@ -0,0 +1,37 @@
+package com.lpc.leetcode;
+
+/**
+ * 打家劫舍IV
+ *
+ * @author byu_rself
+ * @date 2023/9/19 9:54
+ */
+public class P2560 {
+
+    public int minCapability(int[] nums, int k) {
+        int left = 0, right = 0;
+        for (int x : nums) {
+            right = Math.max(right, x);
+        }
+        while (left < right) {
+            int mid = left + right >>> 1;
+            if (check(nums, k, mid)) right = mid;
+            else left = mid + 1;
+        }
+        return right;
+    }
+
+    private boolean check(int[] nums, int k, int max) {
+        int f0 = 0, f1 = 0;
+        for (int x : nums) {
+            if (x > max) {
+                f0 = f1;
+            } else {
+                int tmp = f1;
+                f1 = Math.max(f1, f0 + 1);
+                f0 = tmp;
+            }
+        }
+        return f1 >= k;
+    }
+}

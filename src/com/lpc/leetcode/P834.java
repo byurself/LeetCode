01e025c1e5a915cd04bea91fc6ded81e1149d846
byu_rself <1870287004@qq.com>
2023-09-20 09:59:30 +0800

    [P834]树中距离之和

--- src/com/lpc/leetcode/P834.java
+++ src/com/lpc/leetcode/P834.java
@@ -12,7 +12,52 @@ import java.util.List;
  */
 public class P834 {
 
+    List<Integer>[] g;
+    int[] ans, size;
     int n;
+
+    public int[] sumOfDistancesInTree(int n, int[][] edges) {
+        this.n = n;
+        g = new List[n];
+        Arrays.setAll(g, k -> new ArrayList<>());
+        for (int[] e : edges) {
+            int a = e[0], b = e[1];
+            g[a].add(b);
+            g[b].add(a);
+        }
+        ans = new int[n];
+        size = new int[n];
+        dfs(0, -1, 0);
+        reroot(0, -1);
+        return ans;
+    }
+
+    private void reroot(int x, int pa) {
+        for (int y : g[x]) {
+            if (y != pa) {
+                ans[y] = ans[x] + n - 2 * size[y];
+                reroot(y, x);
+            }
+        }
+    }
+
+    /**
+     * @param x     当前遍历到的节点
+     * @param pa    父节点
+     * @param depth 0~当前节点的深度
+     */
+    private void dfs(int x, int pa, int depth) {
+        ans[0] += depth;
+        size[x] = 1;
+        for (int y : g[x]) {
+            if (y != pa) {
+                dfs(y, x, depth + 1);
+                size[x] += size[y];
+            }
+        }
+    }
+
+    /*int n;
     List<Integer>[] g;
     int[] ans, size;
 
@@ -32,11 +77,11 @@ public class P834 {
         return ans;
     }
 
-    /**
+    *//**
      * @param x     当前遍历到的节点
      * @param pa    父节点
      * @param depth 0~当前节点的深度
-     */
+     *//*
     private void dfs(int x, int pa, int depth) {
         ans[0] += depth;
         size[x] = 1;
@@ -55,5 +100,5 @@ public class P834 {
                 reroot(y, x);
             }
         }
-    }
+    }*/
 }

8508ffc5197c327a1edc5d2e16e34b0e881985cb
byu_rself <1870287004@qq.com>
2023-10-19 09:49:02 +0800

    [P1713]得到子序列的最少操作次数

--- src/com/lpc/leetcode/P1713.java
+++ src/com/lpc/leetcode/P1713.java
@@ -11,23 +11,27 @@ import java.util.*;
 public class P1713 {
 
     public int minOperations(int[] target, int[] arr) {
-        int n = target.length, m = arr.length;
+        int n = target.length;
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) map.put(target[i], i);
+        for (int i = 0; i < n; ++i) map.put(target[i], i);
         List<Integer> list = new ArrayList<>();
-        for (int a : arr) if (map.containsKey(a)) list.add(map.get(a));
+        for (int x : arr) {
+            if (map.containsKey(x)) {
+                list.add(map.get(x));
+            }
+        }
         int length = list.size(), max = 0;
         int[] f = new int[length + 1], g = new int[length + 1];
         Arrays.fill(g, Integer.MAX_VALUE);
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < length; ++i) {
             int index = list.get(i);
-            int l = 0, r = length;
-            while (l < r) {
-                int mid = l + r + 1 >> 1;
-                if (g[mid] < index) l = mid;
-                else r = mid - 1;
+            int left = 0, right = length;
+            while (left < right) {
+                int mid = left + right + 1 >> 1;
+                if (g[mid] < index) left = mid;
+                else right = mid - 1;
             }
-            int t = r + 1;
+            int t = right + 1;
             f[i] = t;
             g[t] = Math.min(g[t], index);
             max = Math.max(max, f[i]);

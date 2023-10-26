9fde1542d1de7fc50b01d0937b42f00d19c310b8
byu_rself <1870287004@qq.com>
2023-10-26 09:52:05 +0800

    [P2049]统计最高分的节点数目

--- src/com/lpc/leetcode/P2049.java
+++ src/com/lpc/leetcode/P2049.java
@@ -1,6 +1,8 @@
 package com.lpc.leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * 统计最高分的节点数目
@@ -10,7 +12,45 @@ import java.util.Arrays;
  */
 public class P2049 {
 
-    static int N = (int) 1e5 + 10, M = N * 2;
+    List<Integer>[] g;
+    int[] f;
+
+    public int countHighestScoreNodes(int[] parents) {
+        int n = parents.length;
+        f = new int[n];
+        g = new List[n];
+        Arrays.setAll(g, k -> new ArrayList<>());
+        for (int i = 1; i < n; ++i) {
+            g[parents[i]].add(i);
+        }
+        dfs(0);
+        long max = 0;
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            long cur = 1;
+            for (int j : g[i]) {
+                cur *= f[j];
+            }
+            if (i != 0) cur *= n - f[i];
+            if (cur > max) {
+                max = cur;
+                ans = 1;
+            } else if (cur == max) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+
+    private int dfs(int i) {
+        int ans = 1;
+        for (int j : g[i]) {
+            ans += dfs(j);
+        }
+        return f[i] = ans;
+    }
+
+    /*static int N = (int) 1e5 + 10, M = N * 2;
     static int[] he = new int[N], e = new int[M], ne = new int[M];
     static int[] f = new int[N];
     int idx;
@@ -46,5 +86,5 @@ public class P2049 {
         int ans = 1;
         for (int i = he[x]; i != -1; i = ne[i]) ans += dfs(e[i]);
         return f[x] = ans;
-    }
+    }*/
 }

05bd760469763a53437f5c6e2b1d0e25e0a99316
byu_rself <1870287004@qq.com>
2023-11-02 10:26:05 +0800

    [P904]水果成篮

--- src/com/lpc/leetcode/P904.java
+++ src/com/lpc/leetcode/P904.java
@@ -9,6 +9,19 @@ package com.lpc.leetcode;
 public class P904 {
 
     public int totalFruit(int[] fruits) {
+        int n = fruits.length, ans = 0, total = 0;
+        int[] cnt = new int[n + 1];
+        for (int i = 0, j = 0; i < n; ++i) {
+            if (++cnt[fruits[i]] == 1) ++total;
+            while (total > 2) {
+                if (--cnt[fruits[j++]] == 0) --total;
+            }
+            ans = Math.max(ans, i - j + 1);
+        }
+        return ans;
+    }
+
+    public int totalFruit1(int[] fruits) {
         int length = fruits.length, ans = 0, total = 0;
         int[] cnt = new int[length + 10];
         for (int i = 0, j = 0; i < length; ++i) {

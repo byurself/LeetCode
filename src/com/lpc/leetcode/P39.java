c56eb37845a2872b9fd0f5ee809773f77b79829d
byu_rself <1870287004@qq.com>
2023-11-13 10:09:09 +0800

    [P39]组合总和

--- src/com/lpc/leetcode/P39.java
+++ src/com/lpc/leetcode/P39.java
@@ -9,26 +9,57 @@ import java.util.*;
  * @date 2022/10/19 12:18
  */
 public class P39 {
-    List<List<Integer>> ans;
+
+    List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    int[] candidates;
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        ans = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        dfs(candidates, target, list, 0);
+        this.candidates = candidates;
+        Arrays.sort(candidates);
+        dfs(0, target);
         return ans;
     }
 
-    private void dfs(int[] candidates, int target, List<Integer> list, int index) {
-        if (index == candidates.length) return;
+    private void dfs(int start, int target) {
         if (target == 0) {
-            ans.add(new ArrayList<>(list));
+            ans.add(new ArrayList<>(path));
             return;
         }
-        dfs(candidates, target, list, index + 1);
-        if (target - candidates[index] >= 0) {
-            list.add(candidates[index]);
-            dfs(candidates, target - candidates[index], list, index);
-            list.remove(list.size() - 1);
+        // 剪枝一：从 start 开始遍历，避免产生重复解
+        for (int i = start; i < candidates.length; ++i) {
+            // 剪枝二：若当前子集和已超过 target，则直接结束循环(因为数组已排序，后面的元素更大，一定会超过 target)
+            if (target - candidates[i] < 0) break;
+            path.add(candidates[i]);
+            dfs(i, target - candidates[i]);
+            path.remove(path.size() - 1);
         }
     }
+
+    // 回溯
+    /*List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    int[] candidates;
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        this.candidates = candidates;
+        dfs(0, target);
+        return ans;
+    }
+
+    private void dfs(int i, int target) {
+        if (i == candidates.length) return;
+        if (target == 0) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        // 不选
+        dfs(i + 1, target);
+        // 选
+        if (target - candidates[i] >= 0) {
+            path.add(candidates[i]);
+            dfs(i, target - candidates[i]);
+            path.remove(path.size() - 1);
+        }
+    }*/
 }

6d4af362c8e9c6f9225de50a5a276a96599db108
byu_rself <1870287004@qq.com>
2023-11-14 10:40:22 +0800

    [P131]分割回文串

--- src/com/lpc/leetcode/P131.java
+++ src/com/lpc/leetcode/P131.java
@@ -3,6 +3,7 @@ package com.lpc.leetcode;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -19,7 +20,82 @@ public class P131 {
         System.out.println(partition(s));
     }
 
+    int[][] f;
     List<List<String>> ans = new ArrayList<>();
+    List<String> path = new ArrayList<>();
+    String s;
+    int n;
+
+    public List<List<String>> partition(String s) {
+        this.s = s;
+        n = s.length();
+        f = new int[n][n];
+        dfs(0);
+        return ans;
+    }
+
+    public void dfs(int i) {
+        if (i == n) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        for (int j = i; j < n; ++j) {
+            if (isPalindrome(i, j) == 1) {
+                path.add(s.substring(i, j + 1));
+                dfs(j + 1);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    // f[i][j] = 0 表示未搜索，1 表示是回文串，-1 表示不是回文串
+    private int isPalindrome(int i, int j) {
+        if (f[i][j] != 0) return f[i][j];
+
+        if (i >= j) f[i][j] = 1;
+        else if (s.charAt(i) == s.charAt(j)) f[i][j] = isPalindrome(i + 1, j - 1);
+        else f[i][j] = -1;
+
+        return f[i][j];
+    }
+
+/*    List<List<String>> ans = new ArrayList<>();
+    List<String> path = new ArrayList<>();
+    boolean[][] f;
+    String s;
+    int n;
+
+    public List<List<String>> partition(String s) {
+        this.s = s;
+        n = s.length();
+        f = new boolean[n][n];
+        for (int i = 0; i < n; ++i) {
+            Arrays.fill(f[i], true);
+        }
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                f[i][j] = (s.charAt(i) == s.charAt(j)) && f[i + 1][j - 1];
+            }
+        }
+        dfs(0);
+        return ans;
+    }
+
+    private void dfs(int i) {
+        if (i == s.length()) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        for (int j = i; j < s.length(); ++j) {
+            if (f[i][j]) {
+                path.add(s.substring(i, j + 1));
+                dfs(j + 1);
+                path.remove(path.size() - 1);
+            }
+        }
+    }*/
+
+    /*List<List<String>> ans = new ArrayList<>();
     List<String> path = new ArrayList<>();
     String s;
 
@@ -48,5 +124,5 @@ public class P131 {
             if (s.charAt(left++) != s.charAt(right--)) return false;
         }
         return true;
-    }
+    }*/
 }

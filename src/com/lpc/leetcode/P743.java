8d90f712efbbc85b1f0225b8a69fafbe5ca783ce
byu_rself <1870287004@qq.com>
2023-11-20 10:50:26 +0800

    [P743]网络延迟时间

--- src/com/lpc/leetcode/P743.java
+++ src/com/lpc/leetcode/P743.java
@@ -14,6 +14,38 @@ public class P743 {
 
     public int networkDelayTime(int[][] times, int n, int k) {
         int[][] g = new int[n][n];
+        for (int[] e : g) {
+            Arrays.fill(e, INF);
+        }
+        for (int[] t : times) {
+            int a = t[0] - 1, b = t[1] - 1, w = t[2];
+            g[a][b] = w;
+        }
+        int[] dist = new int[n];
+        Arrays.fill(dist ,INF);
+        dist[k - 1] = 0;
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; ++i) {
+            int x = -1;
+            for (int j = 0; j < n; ++j) {
+                if (!visited[j] && (x == -1 || dist[x] > dist[j])) {
+                    x = j;
+                }
+            }
+            visited[x] = true;
+            for (int j = 0; j < n; ++j) {
+                dist[j] = Math.min(dist[j], dist[x] + g[x][j]);
+            }
+        }
+        int ans = -1;
+        for (int x : dist) {
+            ans = Math.max(ans, x);
+        }
+        return ans == INF ? -1 : ans;
+    }
+
+    /*public int networkDelayTime(int[][] times, int n, int k) {
+        int[][] g = new int[n][n];
         for (int i = 0; i < n; ++i) Arrays.fill(g[i], INF);
         for (int[] time : times) {
             int x = time[0] - 1, y = time[1] - 1;
@@ -37,5 +69,5 @@ public class P743 {
         }
         int ans = Arrays.stream(dist).max().getAsInt();
         return ans == INF ? -1 : ans;
-    }
+    }*/
 }

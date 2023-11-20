a7cdb5de94edb7a756464f86f7046686052bdef8
byu_rself <1870287004@qq.com>
2023-11-20 10:12:56 +0800

    [P53]最大子数组和

--- src/com/lpc/leetcode/P53.java
+++ src/com/lpc/leetcode/P53.java
@@ -17,25 +17,22 @@ public class P53 {
     }
 
     public int maxSubArray(int[] nums) {
-        int n = nums.length;
+        int n = nums.length, ans = nums[0];
         int[] f = new int[n];
         f[0] = nums[0];
         for (int i = 1; i < n; ++i) {
             if (f[i - 1] > 0) f[i] = f[i - 1] + nums[i];
             else f[i] = nums[i];
-        }
-        int ans = f[0];
-        for (int i = 1; i < n; ++i) {
             ans = Math.max(ans, f[i]);
         }
         return ans;
     }
 
     public int maxSubArray1(int[] nums) {
-        int pre = 0, ans = nums[0];
-        for (int num : nums) {
-            pre = Math.max(pre + num, num);
-            ans = Math.max(ans, pre);
+        int ans = nums[0], f = nums[0];
+        for (int i = 1; i < nums.length; ++i) {
+            f = Math.max(f, 0) + nums[i];
+            ans = Math.max(ans, f);
         }
         return ans;
     }

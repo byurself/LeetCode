14627b4178c1d94fe24c54dbdb786fa97e0daf43
byu_rself <1870287004@qq.com>
2023-11-29 14:14:30 +0800

    [P28]找出字符串中第一个匹配项的下标

--- src/com/lpc/leetcode/P28.java
+++ src/com/lpc/leetcode/P28.java
@@ -12,12 +12,43 @@ public class P28 {
 
     @Test
     public void solution() {
-        String haystack = "aaaaa";
-        String needle = "bba";
+        String haystack = "sadbutsad";
+        String needle = "sad";
         System.out.println(strStr(haystack, needle));
     }
 
     public int strStr(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        // 使下标从 1 开始
+        haystack = " " + haystack;
+        needle = " " + needle;
+        char[] s = haystack.toCharArray(), p = needle.toCharArray();
+        // 求 next 数组，next 数组与模式串（匹配串）相关
+        int[] next = new int[m + 1];
+        for (int i = 1, j = 0; i < m; ) {
+            if (j == 0 || p[i] == p[j]) {
+                ++i;
+                ++j;
+                next[i] = j;
+            } else {
+                j = next[j]; // 匹配失败
+            }
+        }
+        // KMP模式匹配
+        int i = 1, j = 1;
+        while (i <= n && j <= m) {
+            if (j == 0 || s[i] == p[j]) {
+                ++i;
+                ++j;
+            } else {
+                j = next[j];
+            }
+        }
+        if (j > m) return i - m - 1; // 匹配成功
+        else return -1;
+    }
+
+    /*public int strStr(String haystack, String needle) {
         int m = haystack.length(), n = needle.length();
         haystack = " " + haystack;
         needle = " " + needle;
@@ -36,9 +67,9 @@ public class P28 {
             if (j == n) return i - n; // 整串匹配成功
         }
         return -1;
-    }
+    }*/
 
-    public int strStr1(String haystack, String needle) {
+    /*public int strStr1(String haystack, String needle) {
         int m = haystack.length(), n = needle.length();
         for (int i = 0; i + n < m; ++i) {
             boolean flag = true;
@@ -51,5 +82,5 @@ public class P28 {
             if (flag) return i;
         }
         return -1;
-    }
+    }*/
 }

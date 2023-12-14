366f6d8defcc5272e2bc4ffb01c5f1fd92b2fcc8
byu_rself <1870287004@qq.com>
2023-12-14 10:11:24 +0800

    [LCR151]彩灯装饰记录III

---
+++ src/com/lpc/leetcode/LCR151.java
@@ -0,0 +1,52 @@
+package com.lpc.leetcode;
+
+import java.util.*;
+
+/**
+ * 彩灯装饰记录III
+ *
+ * @author byu_rself
+ * @date 2023/12/14 9:56
+ */
+public class LCR151 {
+
+    public List<List<Integer>> decorateRecord(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        if (root != null) queue.offer(root);
+        boolean flag = true;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while (size-- > 0) {
+                TreeNode cur = queue.poll();
+                list.add(cur.val);
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
+            }
+            if (!flag) Collections.reverse(list);
+            ans.add(list);
+            flag = !flag;
+        }
+        return ans;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}

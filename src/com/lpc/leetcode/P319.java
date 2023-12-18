96a3de4a63c2fbe60b66be205569a3ee104e3724
byu_rself <1870287004@qq.com>
2023-12-18 10:43:59 +0800

    [P319]灯泡开关

---
+++ src/com/lpc/leetcode/P319.java
@@ -0,0 +1,13 @@
+package com.lpc.leetcode;
+
+/**
+ * 灯泡开关
+ *
+ * @author byu_rself
+ * @date 2023/12/18 10:06
+ */
+public class P319 {
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}

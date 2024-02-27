0204389f31afb1f94bbde925c7dd0baf50540b84
byu_rself <byu_rself@163.com>
2024-02-27 14:50:54 +0800

    [P2867]统计树中的合法路径数目

---
+++ src/com/lpc/leetcode/P2867.java
@@ -0,0 +1,106 @@
+package com.lpc.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 统计树中的合法路径数目
+ *
+ * @author byu_rself
+ * @date 2024/2/27 11:21
+ */
+public class P2867 {
+
+    private static final PrimeTable PT = new PrimeTable(100010);
+
+    public long countPaths(int n, int[][] edges) {
+        List<Integer>[] g = new List[n + 1];
+        Arrays.setAll(g, i -> new ArrayList<>());
+        UnionFind uf = new UnionFind(n + 1);
+        for (int[] e : edges) {
+            int u = e[0], v = e[1];
+            g[u].add(v);
+            g[v].add(u);
+            if (!PT.isPrime(u) && !PT.isPrime(v)) {
+                uf.union(u, v);
+            }
+        }
+        long ans = 0;
+        for (int i = 1; i <= n; ++i) {
+            if (PT.isPrime(i)) {
+                long t = 0;
+                for (int j : g[i]) {
+                    if (!PT.isPrime(j)) {
+                        long cnt = uf.size(j);
+                        ans += cnt;
+                        ans += cnt * t;
+                        t += cnt;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    private static class PrimeTable {
+        private final boolean[] prime;
+
+        public PrimeTable(int n) {
+            prime = new boolean[n + 1];
+            Arrays.fill(prime, true);
+            prime[0] = false;
+            prime[1] = false;
+            for (int i = 2; i <= n; ++i) {
+                if (prime[i]) {
+                    for (int j = i + i; j <= n; j += i) {
+                        prime[j] = false;
+                    }
+                }
+            }
+        }
+
+        public boolean isPrime(int x) {
+            return prime[x];
+        }
+    }
+
+    private static class UnionFind {
+        private final int[] p;
+        private final int[] size;
+
+        public UnionFind(int n) {
+            p = new int[n];
+            size = new int[n];
+            for (int i = 0; i < n; ++i) {
+                p[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        public int find(int x) {
+            if (p[x] != x) {
+                p[x] = find(p[x]);
+            }
+            return p[x];
+        }
+
+        public void union(int a, int b) {
+            int pa = find(a), pb = find(b);
+            if (pa == pb) {
+                return;
+            }
+            if (size[pa] > size[pb]) {
+                p[pb] = pa;
+                size[pa] += size[pb];
+            } else {
+                p[pa] = pb;
+                size[pb] += size[pa];
+            }
+        }
+
+        public int size(int x) {
+            return size[find(x)];
+        }
+    }
+}
